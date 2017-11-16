3ae5003dab5330523a056d7327bfc2974ffa20b0
Foremanski <33032043+Foremanski@users.noreply.github.com>
2017-11-16 17:12:44 +0000

    Changed Week 3 Exercise 4 files

--- ClassWork/Week 3/Exercise4/Exercise4Javascript.js
+++ ClassWork/Week 3/Exercise4/Exercise4Javascript.js
@@ -1,6 +1,31 @@
 function CreatePlayer()
 {
-document.getElementById('playername').value;
-document.getElementById('name')
-document.name. = playername.innerHTML
+  //PlayerName
+  var playername = document.getElementById('playername').value;
+  document.getElementById('name').innerHTML = playername;
+
+  //PlayerColour
+  var playercolour = document.getElementById('playercolour').value;
+  document.getElementById("colour").style.backgroundColor = playercolour;
+
+  //playerhealth
+  var playerhealth = document.getElementById('playerhealth').value;
+  document.getElementById("health").innerHTML = playerhealth;
+
+  //Playerweapon
+  var Playerweapon = document.getElementById('Playerweapon').value;
+
+  if (Playerweapon == 1)
+  {
+  Playerweapon = 1;
+  }
+
+  else if (Playerweapon  == 2)
+  {
+  Playerweapon = 2;
+  }
+
+  else {weapon = 3}
+
+    document.getElementById("weapon").innerHTML = Playerweapon;
 }
