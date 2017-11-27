1539597246928c76bc46896e0f28d055fcdf2a3e
Foremanski <33032043+Foremanski@users.noreply.github.com>
2017-11-27 17:49:58 +0000

    Game and Website
    Started Work on my choice game, in here is the plan for it

---
+++ ClassWork/Game & Website Work/Website Game/data.js
@@ -0,0 +1,12 @@
+var roomArray = [
+	{
+		title:'Crash',
+		text:'You wake up ',
+		choices:[
+			{
+				text:'Go to room 2',
+				index:1
+			}
+		]
+	},
+]
---
+++ ClassWork/Game & Website Work/Website Game/main.js
@@ -0,0 +1,30 @@
+//Website
+
+
+
+
+
+
+
+//Engine for multiple choices
+function OnLoad()
+{
+	SelectRoom(0);
+}
+
+function SelectRoom(roomIndex)
+{
+	document.getElementById('roomTitle').innerHTML =  roomArray[roomIndex].title;
+	document.getElementById('roomText').innerHTML = roomArray[roomIndex].text;
+	document.getElementById('roomChoices').innerHTML = " ";
+
+	for(var i = 0; i < roomArray[roomIndex].choices.length; i++)
+	{
+		var roomChoices = "<button type='button' onclick='SelectRoom("
+		+ roomArray[roomIndex].choices[i].index
+		+ ")'>"
+    + roomArray[roomIndex].choices[i].text
+		+ "</button>";
+		document.getElementById("roomChoices").innerHTML += roomChoices;
+	}
+}
