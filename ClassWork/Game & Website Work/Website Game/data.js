c1506f1bf953a9cb8d3feab8ed054406f693d22b
Foremanski <33032043+Foremanski@users.noreply.github.com>
2018-01-11 13:01:56 +0000

    Started implementing choices in game
    Started working on the website.

--- ClassWork/Game & Website Work/Website Game/data.js
+++ ClassWork/Game & Website Work/Website Game/data.js
@@ -1,12 +1,90 @@
 var roomArray = [
 	{
-		title:'Crash',
-		text:'You wake up ',
+		title:'Start Game',
+		text:' Begin Game?',
 		choices:[
 			{
-				text:'Go to room 2',
+				text:'Crash',
 				index:1
 			}
 		]
 	},
+	{
+		title: 'Crash',
+		text: 'You wake up in a crashed pod, luckily you managed to survive the crash but the wilderness of this planet is to likely kill you.',
+		choices: [
+			{
+				text:'Pull emergency lever',
+				index:2
+			}
+		]
+	},
+	{
+ 	 title: 'Pull emergency lever',
+ 	 text: 'With the pod behind you, you step onto a rocky cliffside. Below you is a dense jungle, but you will need something to climb down with. To the left of you is a lighter and less dense forest. You could also search the surrounding area as well.',
+ 	 Choices: [
+ 		 {
+ 		 text: 'Search the Cliffside'
+ 		 index: 18;
+ 	   }
+		 {
+			 text: 'Proceed to Forest'
+			 index: 4;
+		 }
+		 {
+			 text: 'Proceed to Jungle (Requires vine to proceed)'
+	 		 index: 3;
+		 }
+ 	 ]
+  }
+	{
+ 	 title:
+ 	 text:
+ 	 Choices: [
+ 		 {
+ 		 text:
+ 		 index:
+ 	   }
+ 	 ]
+  }
+  {
+ 	title:
+ 	text:
+ 	Choices: [
+ 		{
+ 		text:
+ 		index:
+ 		}
+ 	]
+  }
+  {
+ 	 title:
+ 	 text:
+ 	 Choices: [
+ 		 {
+ 		 text:
+ 		 index:
+ 	   }
+ 	 ]
+  }
+  {
+ 	 title:
+ 	 text:
+ 	 Choices: [
+ 		 {
+ 		 text:
+ 		 index:
+ 	   }
+ 	 ]
+  }
+  {
+ 	title:
+ 	text:
+ 	Choices: [
+ 		{
+ 		text:
+ 		index:
+ 		}
+ 	]
+ }
 ]
